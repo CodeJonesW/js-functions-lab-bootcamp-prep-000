d4162253583c6861584d5bb123e2ddab22bd3eac
Will Jones <willjones2010@gmail.com>
2019-02-26 03:28:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function happyHolidayTo () {
   var holiday = 'Independence Day';
   return `Happy ${holiday}, ${name}!`;
 }
-var days = '20';
-var holiday = 'Mother\'s Day';
 function holidayCountdown() {
+  var days = '20';
+  var holiday = 'Mother\'s Day';
   return `It's ${days} days until ${holiday}!`;
 }
